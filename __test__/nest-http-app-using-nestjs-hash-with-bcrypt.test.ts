630e3f8ebcf6811865bbd88d48f9b114b9ffd17e
Arthur reis <arthurreis074@gmail.com>
2022-03-29 12:48:07 -0300

    chore: added e2e tests

---
+++ __test__/nest-http-app-using-nestjs-hash-with-bcrypt.test.ts
@@ -0,0 +1,73 @@
+import 'reflect-metadata';
+import * as bcrypt from 'bcrypt';
+
+import {
+  Controller,
+  INestApplication,
+  Injectable,
+  Module,
+  Post,
+} from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import request from 'supertest';
+
+import { HashModule } from '../lib/hash.module';
+import { HashService } from '../lib/hash.service';
+
+const MESSAGE = 'Hello World!';
+
+@Injectable()
+class TestService {
+  constructor(private readonly hashService: HashService) {}
+
+  public async makeHash() {
+    return {
+      hash: this.hashService.hashSync(MESSAGE),
+    };
+  }
+}
+
+@Controller()
+class TestController {
+  constructor(private readonly testService: TestService) {}
+
+  @Post()
+  public async testFunction() {
+    return this.testService.makeHash();
+  }
+}
+
+@Module({
+  imports: [
+    HashModule.forRoot({
+      type: 'bcrypt',
+      rounds: 2,
+    }),
+  ],
+  controllers: [TestController],
+  providers: [TestService],
+})
+class TestModule {}
+
+describe('Using lib nestjs-hash with md5 algorithm', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [TestModule],
+    }).compile();
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should be able create and return hash with algorithm bcrypt', async () => {
+    const response = await request(app.getHttpServer()).post('/');
+
+    expect(response.status).toBe(201);
+    expect(bcrypt.compareSync(MESSAGE, response.body.hash)).toEqual(true);
+  });
+});
---
+++ __test__/nest-http-app-using-nestjs-hash.with-md5.test.ts
@@ -0,0 +1,74 @@
+import 'reflect-metadata';
+
+import {
+  Controller,
+  INestApplication,
+  Injectable,
+  Module,
+  Post,
+} from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import request from 'supertest';
+
+import { HashModule } from '../lib/hash.module';
+import { HashService } from '../lib/hash.service';
+import { createHash } from 'crypto';
+
+const MESSAGE = 'Hello World!';
+
+@Injectable()
+class TestService {
+  constructor(private readonly hashService: HashService) {}
+
+  public async makeHash() {
+    return {
+      hash: this.hashService.hashSync(MESSAGE),
+    };
+  }
+}
+
+@Controller()
+class TestController {
+  constructor(private readonly testService: TestService) {}
+
+  @Post()
+  public async testFunction() {
+    return this.testService.makeHash();
+  }
+}
+
+@Module({
+  imports: [
+    HashModule.forRoot({
+      type: 'md5',
+    }),
+  ],
+  controllers: [TestController],
+  providers: [TestService],
+})
+class TestModule {}
+
+describe('Using lib nestjs-hash with md5 algorithm', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [TestModule],
+    }).compile();
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should be able create and return hash with algorithm md5', async () => {
+    const md5Hash = createHash('md5').update(MESSAGE).digest('hex');
+
+    const response = await request(app.getHttpServer()).post('/');
+
+    expect(response.status).toBe(201);
+    expect(response.body.hash).toBe(md5Hash);
+  });
+});
