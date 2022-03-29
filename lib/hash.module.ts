0d5d9df671295c73d787bbc803e9d25e6902ea5f
Arthur reis <arthurreis074@gmail.com>
2022-03-29 12:47:49 -0300

    feat: added hash module

--- lib/hash.module.ts
+++ lib/hash.module.ts
@@ -1,14 +1,33 @@
-import { ModuleMetadata } from '@nestjs/common';
-import { HashModuleOptions } from './interfaces/module';
-import { NativeHashProvider } from './interfaces/provider';
+import { DynamicModule } from '@nestjs/common';
+
+import { HASH_PROVIDER_OPTIONS, HASH_PROVIDER } from '@lib/data/tokens';
+
+import { HashType } from '@lib/types/hash-type';
+import { HashModuleOptions } from '@lib/types/hash-module-options';
+
+import { HashService } from '@lib/hash.service';
+
+import { NativeHashProvider } from '@lib/providers/native.provider';
+import { BcryptProvider } from '@lib/providers/bcrypt.provider';
 
 export class HashModule {
-  forFeature(options: HashModuleOptions): ModuleMetadata {
-    const isNative = options.hash in NativeHashProvider;
-    const hashProvider = isNative ? NativeHashProvider[options.hash] : null;
+  public static forRoot(options: HashModuleOptions): DynamicModule {
+    if (!options.type) throw new Error('Hash type is required');
+    const HashProvider = HashModule.getHashProviderByType(options.type);
 
     return {
-      providers: [hashProvider],
+      module: HashModule,
+      providers: [
+        { provide: HASH_PROVIDER_OPTIONS, useValue: { ...options } },
+        { provide: HASH_PROVIDER, useClass: HashProvider },
+        HashService,
+      ],
+      exports: [HashService],
     };
+  }
+
+  private static getHashProviderByType(hashType: HashType) {
+    if (hashType === 'bcrypt') return BcryptProvider;
+    return NativeHashProvider;
   }
 }
